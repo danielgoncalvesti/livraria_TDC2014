8734668874faa0edc1a0f91a3a0027a496705ea8
Priscila Carvalho <pri.carvalho86@gmail.com>
2014-08-08 11:41:38 -0300

    ultimos ajustes do projeto para a palestra

--- livraria/build.sbt
+++ livraria/build.sbt
@@ -7,11 +7,24 @@ scalaVersion := "2.10.4"
 scalaBinaryVersion := "2.10"
 
 libraryDependencies ++= Seq(
-  jdbc,
-  anorm,
-  cache
-)     
+ 		jdbc,
+  		cache,
+		"com.typesafe.slick" %% "slick" % "2.1.0-RC3",
+		"com.h2database" % "h2" % "1.3.175",
+		"com.typesafe" % "config" % "1.2.0",
+		"com.typesafe.play" %% "play" % "2.2.2" withSources,
+		"com.typesafe.play" %% "play-json" % "2.3.2" withSources,
+		"jp.t2v" %% "play2-auth" % "0.11.0" withSources,
+		"jp.t2v" %% "play2-auth-test" % "0.11.0" % "test" withSources,
+		"com.typesafe" %% "scalalogging-slf4j" % "1.0.1" withSources,
+		"com.typesafe" % "config" % "1.0.1" withSources,
+		"junit" % "junit" % "4.8.1" % "test",
+		"org.mockito" % "mockito-core" % "1.9.5" % "test",
+		"net.liftweb" %% "lift-json" % "2.5.1",
+		"org.specs2" %% "specs2" % "2.3.7" % "test"
+)
+     
+instrumentSettings
 
 play.Project.playScalaSettings
 
-instrumentSettings
--- livraria/project/Build.scala
+++ livraria/project/Build.scala
@@ -15,23 +15,7 @@ object ApplicationBuild extends Build {
 	val appName = "livraria"
 	val appVersion = "1.0-SNAPSHOT"
 
-	val dependencies = Seq(
-		"com.typesafe.slick" %% "slick" % "2.1.0-RC3",
-		"com.h2database" % "h2" % "1.3.175",
-		"com.typesafe" % "config" % "1.2.0",
-		"com.typesafe.play" %% "play" % "2.2.2" withSources,
-		"com.typesafe.play" %% "play-json" % "2.3.2" withSources,
-		"jp.t2v" %% "play2-auth" % "0.11.0" withSources,
-		"jp.t2v" %% "play2-auth-test" % "0.11.0" % "test" withSources,
-		"com.typesafe" %% "scalalogging-slf4j" % "1.0.1" withSources,
-		"com.typesafe" % "config" % "1.0.1" withSources,
-		"junit" % "junit" % "4.8.1" % "test",
-		"org.mockito" % "mockito-core" % "1.9.5" % "test",
-		"net.liftweb" %% "lift-json" % "2.5.1",
-		"org.specs2" %% "specs2" % "2.3.7" % "test"
-		)
-
-	val main = play.Project(appName, appVersion, dependencies).settings(
+	val main = play.Project(appName, appVersion).settings(
 		//Filtrando execucao de classes de teste de por nome de classe
 		testOptions in Test := Seq(Tests.Filter(_ endsWith "Spec")),
 		//Configuraçoes de setup de execucao dos testes
